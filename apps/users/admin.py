1279e0697d46d13fac1663a7f3e0f3e305fabfe2
Iri O <oparina.iri@gmail.com>
2024-05-21 19:24:11 +0500

    Added User model

---
+++ apps/users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [
+        'email',
+        'is_superuser',
+        'email_confirmed',
+    ]
