4829d1b556a369d420d6a53dcb880e0a3931ff31
Iri O <oparina.iri@gmail.com>
2024-05-23 19:25:54 +0500

    Added note model to admin

---
+++ apps/notes/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'created_at',
+    ]
