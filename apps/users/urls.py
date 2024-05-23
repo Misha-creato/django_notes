5315656f109108d9e06b8676d5ab7f329aa10f78
Iri O <oparina.iri@gmail.com>
2024-05-23 15:43:54 +0500

    Added new paths

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from users.views import (
     LoginView,
+    LogoutView,
     RegisterView,
     ConfirmEmailView,
+    SendConfirmEmailView,
+    SettingsView,
+    PasswordResetRequestView,
+    PasswordResetView,
 )
 
 
@@ -14,6 +19,11 @@ urlpatterns = [
         name='login',
     ),
     path(
+        'logout/',
+        LogoutView.as_view(),
+        name='logout',
+    ),
+    path(
         'register/',
         RegisterView.as_view(),
         name='register',
@@ -22,5 +32,25 @@ urlpatterns = [
         'confirm_email/<str:url_hash>/',
         ConfirmEmailView.as_view(),
         name='confirm_email',
+    ),
+    path(
+        'send_confirm_email/',
+        SendConfirmEmailView.as_view(),
+        name='send_confirm_email',
+    ),
+    path(
+        'settings/',
+        SettingsView.as_view(),
+        name='settings',
+    ),
+    path(
+        'password/reset/request/',
+        PasswordResetRequestView.as_view(),
+        name='password_reset_request',
+    ),
+    path(
+        'password/reset/<str:url_hash>/',
+        PasswordResetView.as_view(),
+        name='password_reset',
     ),
 ]
