a0004bed6d1080a720c39b49c43a148a0532fdc7
Iri O <oparina.iri@gmail.com>
2024-05-23 19:25:39 +0500

    Added NoteForm

---
+++ apps/notes/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from notes.models import Note
+
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = [
+            'title',
+            'description',
+            'notify_at',
+        ]
