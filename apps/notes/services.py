72d1ee9d5a744deef2e9033fabaf9bc9df4e6697
Iri O <oparina.iri@gmail.com>
2024-05-24 18:15:38 +0500

    Changed logs

--- apps/notes/services.py
+++ apps/notes/services.py
@@ -9,15 +9,15 @@ from users.services import set_form_messages
 
 
 def get_note(request: Any, slug: str) -> (int, Note | None):
-    print(f'Поиск заметки {slug}')
     author = request.user
+    print(f'Поиск заметки {slug} пользователя {author}')
     try:
         note = Note.objects.filter(
             author=author,
             slug=slug,
         ).first()
     except Exception as exc:
-        print(f'Ошибка при попытке найти заметку {exc}')
+        print(f'Ошибка при попытке найти заметку {slug} пользователя {author}: {exc}')
         messages.error(
             request=request,
             message='Возникла ошибка',
@@ -25,14 +25,14 @@ def get_note(request: Any, slug: str) -> (int, Note | None):
         return 500, None
 
     if note is None:
-        print(f'Не удалось найти заметку {slug}')
+        print(f'Не удалось найти заметку {slug} пользователя {author}')
         messages.error(
             request=request,
             message='Не удалось найти заметку',
         )
         return 404, None
 
-    print('Заметка найдена')
+    print(f'Заметка {slug} пользователя {author} найдена')
     return 200, note
 
 
@@ -44,24 +44,24 @@ def edit_note(request: Any, slug: str) -> (int, str):
     if status != 200:
         return status, None
 
-    status, slug = edit_or_create_note(
+    print(f'Редактирование заметки {slug} с данными {request.POST}')
+    status, note = edit_or_create_note(
         request=request,
         note=note,
     )
 
-    if status != 200:
-        return status, note.slug
-
-    print(f'Заметка {note} изменена')
-    messages.success(
-        request=request,
-        message='Заметка успешно изменена',
-    )
-    return status, slug
+    if status == 200:
+        print(f'Заметка {note} пользователя {request.user} изменена')
+        messages.success(
+            request=request,
+            message='Заметка успешно изменена',
+        )
+    return status, note.slug
 
 
-def create_note(request: Any) -> (int, str):
-    status, slug = edit_or_create_note(
+def create_note(request: Any) -> int:
+    print(f'Cоздание заметки {request.POST} пользователем {request.user}')
+    status, note = edit_or_create_note(
         request=request,
     )
     if status == 200:
@@ -69,12 +69,13 @@ def create_note(request: Any) -> (int, str):
             request=request,
             message='Заметка успешно создана',
         )
-    return status, slug
+
+    print(f'Заметка {note} пользователя {request.user} успешно создана')
+    return status
 
 
-def edit_or_create_note(request: Any, note: Note = None) -> (int, str):
+def edit_or_create_note(request: Any, note: Note = None) -> (int, Note):
     data = request.POST
-    print(f'Редактирование или создание заметки {data}')
     form = NoteForm(data, note)
     if not form.is_valid():
         print(f'Невалидные данные {form.errors}')
@@ -84,17 +85,18 @@ def edit_or_create_note(request: Any, note: Note = None) -> (int, str):
         )
         return 400, None
 
+    form.instance.author = request.user
     try:
-        form.instance.author = request.user
         form.save()
     except Exception as exc:
-        print(f'Возникла ошибка при сохранении изменений заметки {exc}')
+        print(f'Возникла ошибка при сохранении изменений заметки {form.instance} пользователя {request.user}: {exc}')
         messages.error(
             request=request,
             message='Возникла ошибка',
         )
         return 500, None
-    return 200, form.instance.slug
+
+    return 200, form.instance
 
 
 def delete_note(request: Any, slug: str) -> int:
@@ -105,17 +107,18 @@ def delete_note(request: Any, slug: str) -> int:
     if status != 200:
         return status
 
+    print(f'Удаление заметки {note} пользователя {request.user}')
     try:
         note.delete()
     except Exception as exc:
-        print(f'Возникла ошибка при удалении заметки {exc}')
+        print(f'Возникла ошибка при удалении заметки {note} пользователя {request.user}: {exc}')
         messages.error(
             request=request,
             message='Не удалось удалить заметку',
         )
         return 500
 
-    print(f'Заметка успешно удалена')
+    print(f'Заметка {note} пользователя {request.user} успешно удалена')
     messages.success(
         request=request,
         message='Заметка успешно удалена',
