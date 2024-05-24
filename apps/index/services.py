3a292eb9393e711deb331ede3a4a1d2b6dbbd857
Iri O <oparina.iri@gmail.com>
2024-05-24 18:13:36 +0500

    Updated get_notes

--- apps/index/services.py
+++ apps/index/services.py
@@ -5,16 +5,18 @@ from django.contrib import messages
 from notes.models import Note
 
 
-def get_notes(request: Any) -> Note | None:
+def get_notes(request: Any) -> [Note]:
+    print(f'Получение списка заметок пользователя {request.user}')
     try:
         notes = Note.objects.filter(
             author=request.user,
         )
     except Exception as exc:
-        print(f'Возникла ошибка при получении списка заметок {exc}')
+        print(f'Возникла ошибка при получении списка заметок пользователя {request.user}: {exc}')
         messages.error(
             request=request,
             message='Возникла ошибка',
         )
-        return None
+        return []
+    print(f'Список заметок пользователя {request.user} получен')
     return notes
