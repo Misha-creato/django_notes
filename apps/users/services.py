b6be1fce26b29158efa25d997e189ecb7b46d3bf
Iri O <oparina.iri@gmail.com>
2024-05-27 19:24:50 +0500

    Updated logs

--- apps/users/services.py
+++ apps/users/services.py
@@ -14,7 +14,7 @@ from django.contrib.auth.forms import (
 )
 from django.urls import reverse
 
-from notifications.services import send_email_by_action
+from notifications.services import send_email_by_type
 
 from users.forms import (
     CustomUserCreationForm,
@@ -26,10 +26,11 @@ from users.models import CustomUser
 
 def register_user(request: Any) -> int:
     data = request.POST
-    print(f'Регистрация пользователя {data}')
+    email = data.get('email')
+    print(f'Регистрация пользователя {email}')
     form = CustomUserCreationForm(data)
     if not form.is_valid():
-        print(f'Невалидные данные {form.errors}')
+        print(f'Невалидные данные для регистрации пользователя {email}: {form.errors}')
         set_form_messages(
             request=request,
             form=form,
@@ -62,10 +63,11 @@ def register_user(request: Any) -> int:
 
 def login_user(request: Any) -> int:
     data = request.POST
-    print(f'Вход пользователя {data}')
+    email = data.get('email')
+    print(f'Вход пользователя {email}')
     form = LoginForm(data)
     if not form.is_valid():
-        print(f'Невалидные данные {form.errors}')
+        print(f'Невалидные данные для входа пользователя {email}: {form.errors}')
         set_form_messages(
             request=request,
             form=form,
@@ -82,7 +84,7 @@ def login_user(request: Any) -> int:
             request=request,
             message='Неправильные адрес электронной почты или пароль'
         )
-        print(f'Ошибка аутентификации пользователя {data}')
+        print(f'Ошибка аутентификации пользователя {email}')
         return 401
 
     login(
@@ -94,7 +96,7 @@ def login_user(request: Any) -> int:
 
 
 def confirm_email(request: Any, url_hash: str) -> int:
-    print(f'Подтверждение адреса электронной почты {url_hash}')
+    print(f'Подтверждение адреса электронной почты польхователя с хэшем: {url_hash}')
     status, user = get_user_by_hash(
         request=request,
         url_hash=url_hash,
@@ -107,7 +109,7 @@ def confirm_email(request: Any, url_hash: str) -> int:
     try:
         user.save()
     except Exception as exc:
-        print(f'Возникла ошибка при сохранении изменений полей email_confirmed, url_hash у пользователя {user}: {exc}')
+        print(f'Возникла ошибка при подтверждении адреса электронной почты пользователя {user}: {exc}')
         messages.error(
             request=request,
             message='Возникла ошибка при подтверждении адреса электронной почты',
@@ -125,10 +127,10 @@ def confirm_email(request: Any, url_hash: str) -> int:
 def change_password(request: Any) -> int:
     data = request.POST
     user = request.user
-    print(f'Настройки изменения пароля {data} для пользователя {user}')
+    print(f'Настройки изменения пароля для пользователя {user}')
     form = PasswordChangeForm(data=data, user=user)
     if not form.is_valid():
-        print(f'Невалидные данные {form.errors}')
+        print(f'Невалидные данные для смены пароля пользователя {user}: {form.errors}')
         set_form_messages(
             request=request,
             form=form,
@@ -138,7 +140,7 @@ def change_password(request: Any) -> int:
     try:
         form.save()
     except Exception as exc:
-        print(f'Возникла ошибка при изменении пароля {data} пользователя {user}: {exc}')
+        print(f'Возникла ошибка при изменении пароля пользователя {user}: {exc}')
         messages.error(
             request=request,
             message='Возникла ошибка при изменении пароля',
@@ -162,7 +164,7 @@ def password_reset_request(request: Any) -> int:
     print(f'Запрос на восстановление пароля {data}')
     form = PasswordResetRequestForm(data)
     if not form.is_valid():
-        print(f'Невалидные данные {form.errors}')
+        print(f'Невалидные данные для запроса на восстановление пароля: {form.errors}')
         set_form_messages(
             request=request,
             form=form,
@@ -196,6 +198,7 @@ def password_reset_request(request: Any) -> int:
 
 
 def password_reset(request: Any, url_hash: str) -> int:
+    print(f'Сброс пароля для пользователя c хэшем: {url_hash}')
     status, user = get_user_by_hash(
         request=request,
         url_hash=url_hash,
@@ -204,13 +207,12 @@ def password_reset(request: Any, url_hash: str) -> int:
         return status
 
     data = request.POST
-    print(f'Сброс пароля для пользователя {user}')
     form = SetPasswordForm(
         data=data,
         user=user,
     )
     if not form.is_valid():
-        print(f'Невалидные данные {form.errors}')
+        print(f'Невалидные данные для сброса пароля пользователя {user}: {form.errors}')
         set_form_messages(
             request=request,
             form=form,
@@ -261,10 +263,11 @@ def get_user_by_hash(request: Any, url_hash: str) -> (int, CustomUser | None):
 
 
 def send_confirmation_email(request: Any, user: CustomUser) -> int:
+    print(f'Подготовка отправки письма для подтверждения адреса электронной почты пользователю {user}')
     status = get_mail_response_status(
         request=request,
         user=user,
-        action='confirm_email',
+        email_type='confirm_email',
     )
     if status != 200:
         messages.error(
@@ -288,10 +291,11 @@ def send_confirmation_email(request: Any, user: CustomUser) -> int:
 
 
 def send_password_reset_email(request: Any, user: CustomUser) -> int:
+    print(f'Подготовка отправки письма для сброса пароля пользователю {user}')
     status = get_mail_response_status(
         request=request,
         user=user,
-        action='password_reset',
+        email_type='password_reset',
     )
     if status != 200:
         messages.error(
@@ -307,17 +311,17 @@ def send_password_reset_email(request: Any, user: CustomUser) -> int:
     return 200
 
 
-def get_mail_response_status(request: Any, user: CustomUser, action: str) -> int:
+def get_mail_response_status(request: Any, user: CustomUser, email_type: str) -> int:
     status, mail_data = get_mail_data(
         request=request,
         user=user,
-        action=action,
+        email_type=email_type,
     )
     if status == 200:
-        status = send_email_by_action(
+        status = send_email_by_type(
             user=user,
             mail_data=mail_data,
-            action=action,
+            email_type=email_type,
         )
         if status == 403:
             messages.warning(
@@ -327,31 +331,39 @@ def get_mail_response_status(request: Any, user: CustomUser, action: str) -> int
     return status
 
 
-def get_mail_data(request: Any, user: CustomUser, action: str) -> (int, dict | None):
-    print(f'Получение данных для формирования текста письма {action} пользователю {user}')
+def get_mail_data(request: Any, user: CustomUser, email_type: str) -> (int, dict | None):
+    print(f'Получение данных для формирования текста письма {email_type} пользователю {user}')
     status, url_hash = set_url_hash(
         user=user,
     )
     if status != 200:
+        print(f'Не удалось получить данные для формирования '
+              f'текста письма {email_type} пользователю {user}'
+              )
         return status, None
 
-    url = request.build_absolute_uri(reverse(action, args=(url_hash,)))
+    url = request.build_absolute_uri(reverse(email_type, args=(url_hash,)))
     mail_data = {
         'url': url,
     }
 
+    print(f'Данные для формирования текста письма {email_type} '
+          f'пользователю {user} получены: {mail_data}'
+          )
     return 200, mail_data
 
 
 def set_url_hash(user: CustomUser) -> (int, str):
-    print(f'Получение хэша для пользователя {user}')
+    print(f'Установка хэша для пользователя {user}')
     url_hash = str(uuid.uuid4())
     user.url_hash = url_hash
+
     try:
         user.save()
     except Exception as exc:
         print(f'Не удалось установить хэш для пользователя {user}: {exc}')
         return 500, None
+
     print(f'Хэш для пользователя {user} установлен')
     return 200, url_hash
 
