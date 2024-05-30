b658cb2200434bcbf330731fd2a9c57306e6eaea
Iri O <oparina.iri@gmail.com>
2024-05-30 18:29:54 +0500

    Added EmailConfig model

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from solo.models import SingletonModel
 
 from utils.constants import EMAIL_TYPES
 
@@ -25,3 +26,17 @@ class EmailTemplate(models.Model):
         db_table = 'email_templates'
         verbose_name = 'Шаблон письма'
         verbose_name_plural = 'Шаблоны писем'
+
+
+class EmailConfiguration(SingletonModel):
+    send_emails = models.BooleanField(
+        verbose_name='Отправка писем включена',
+        default=True,
+    )
+
+    def __str__(self):
+        return ''
+
+    class Meta:
+        db_table = 'email_configurations'
+        verbose_name = 'Настройки email'
