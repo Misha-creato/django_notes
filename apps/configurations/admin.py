78b19d8f6be0e0bb7558f57ab74edc5dba266a63
Iri O <oparina.iri@gmail.com>
2024-05-30 18:27:59 +0500

    Added date validator

--- apps/configurations/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from configurations.models import Configuration
-from solo.admin import SingletonModelAdmin
-
-
-admin.site.register(Configuration, SingletonModelAdmin)
--- apps/notes/forms.py
+++ apps/notes/forms.py
@@ -1,9 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 from notes.models import Note
 
 
+def validate_notify_date(value):
+    min_date = timezone.now()
+    if value < min_date:
+        raise ValidationError(
+            'Дата и время оповещения не могут быть в прошлом.'
+        )
+
+
 class NoteForm(forms.ModelForm):
+    notify_at = forms.DateTimeField(
+        validators=[validate_notify_date]
+    )
+
     class Meta:
         model = Note
         fields = [
